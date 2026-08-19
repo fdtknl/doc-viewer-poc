cb0efd834e8c467108a5c550b7f08dbdfb764253
fdtknl <m.denijs@tweedekamer.nl>
2026-08-19 07:26:49 +0000

    moved app to route and started on onlyoffice page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <a href="/onlyoffice">Open OnlyOffice PoC</a>
+    </div>
+  )
+}
---
+++ app/pages/onlyoffice.tsx
@@ -0,0 +1,46 @@
+// https://api.onlyoffice.com/docs/docs-api/get-started/frontend-frameworks/react/
+import {DocumentEditor} from "@onlyoffice/document-editor-react";
+import React, {useRef} from "react";
+
+function onDocumentReady(event) {
+  console.log("Document is loaded");
+}
+
+function onLoadComponentError(errorCode, errorDescription) {
+  switch (errorCode) {
+  case -1: // Unknown error loading component
+    console.log(errorDescription);
+    break;
+
+  case -2: // Error load DocsAPI from http://documentserver/
+    console.log(errorDescription);
+    break;
+
+  case -3: // DocsAPI is not defined
+    console.log(errorDescription);
+    break;
+  }
+}
+
+export default function OnlyOffice() {
+  return (
+    <DocumentEditor
+      id="docxEditor"
+      documentServerUrl="http://localhost:80/"
+      config={{
+        document: {
+          fileType: "docx",
+          key: "Khirz6zTPdfd7",
+          title: "Example Document Title.docx",
+          url: "https://example.com/url-to-example-document.docx",
+        },
+        documentType: "word",
+        editorConfig: {
+          callbackUrl: "https://example.com/url-to-callback",
+        },
+      }}
+      events_onDocumentReady={onDocumentReady}
+      onLoadComponentError={onLoadComponentError}
+    />
+  )
+}
