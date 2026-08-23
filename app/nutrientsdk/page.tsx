10a548f077e77a18425f69cfc698fd465bdf41d5
fdtknl <m.denijs@tweedekamer.nl>
2026-08-23 16:44:48 +0000

    using xfdf instead of intantjson in nutrientsdk

--- app/nutrientsdk/page.tsx
+++ app/nutrientsdk/page.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useRef } from "react";
 
 export default function nutrientSdk() {
     const containerRef = useRef(null);
-    const [localStorage, setLocalStorage] = useLocalStorage<InstantJSON>('nutrientsdk');
+    const [localStorage, setLocalStorage] = useLocalStorage<string>('nutrientsdk_xfdf');
 
     useEffect(() => {
         const container = containerRef.current;
@@ -25,43 +25,44 @@ export default function nutrientSdk() {
                     useCDN: true,
                     document:
                         "Hello World.pdf",
-                    instantJSON: localStorage
+                    XFDF: localStorage,
+                    // instantJSON: localStorage
                 }).then(async (instance) => {
                     instance.addEventListener("annotations.create", async createdAnnotations => {
                         console.log("annotations.create", createdAnnotations, instance.annotations);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                     instance.addEventListener("annotations.update", async updatedAnnotations => {
                         console.log("annotations.update", updatedAnnotations);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                     instance.addEventListener("annotations.delete", async deletedAnnotations => {
                         console.log("annotations.update", deletedAnnotations);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                     instance.addEventListener("comments.create", async createdComments => {
                         console.log("comments.create", createdComments);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                     instance.addEventListener("comments.update", async updatedComments => {
                         console.log("comments.update", updatedComments);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                     instance.addEventListener("comments.delete", async deletedComments => {
                         console.log("comments.update", deletedComments);
-                        const exportedInstantJson = await instance.exportInstantJSON();
-                        console.log("exportedInstantJson", exportedInstantJson)
-                        setLocalStorage(exportedInstantJson);
+                        const exportedXfdf = await instance.exportXFDF();
+                        console.log("exportedXfdf", exportedXfdf)
+                        setLocalStorage(exportedXfdf);
                     });
                 });
             }
